7e2415fc62a1ed30714fabbb07d24f2c1d7acbf4
olvlesya <lesyonoknou@gmail.com>
2020-11-17 17:31:38 +0100

    feat: implement filtering-logic

---
+++ src/components/Searching/Searching.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { Input } from "antd";
+
+const { Search } = Input;
+
+type Props = {
+  onSearch: (value: string) => void;
+};
+export const Searching: React.FunctionComponent<Props> = ({ onSearch }) => {
+  const [value, setValue] = useState("");
+  return (
+    <div>
+      <Search
+        placeholder="Find todo"
+        onSearch={onSearch}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+    </div>
+  );
+};
